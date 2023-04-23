31aa7e78a831fa5f10c8c0eefa035c40652d6494
yunhwa25 <yun11258@naver.com>
2023-04-23 22:53:07 +0900

    수정

--- react-shop/src/components/ListContainer.tsx
+++ react-shop/src/components/ListContainer.tsx
@@ -1,9 +1,11 @@
+import { CATEGORY } from "../common/constants";
 import styles from "./ListContainer.module.css";
 import ListItem from "./ListItem";
 
 export default function ListContainer() {
   return (
     <>
+      <h2>CATEGORY</h2>
       <ListItem />
     </>
   );
